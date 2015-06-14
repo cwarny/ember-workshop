8942e0fa28e8e75de8fa40e0f5629ff4f1d5d4e9
Cedric Warny <cedric.warny@gmail.com>
2015-06-14 10:17:24 -0400

    first commit

--- config/environment.js
+++ config/environment.js
@@ -19,6 +19,16 @@ module.exports = function(environment) {
     }
   };
 
+  ENV.contentSecurityPolicy = {
+    'default-src': "'none'",
+    'script-src': "'self'",
+    'font-src': "'self'",
+    'connect-src': "'self'",
+    'img-src': "'self' http://upload.wikimedia.org http://bl.ocks.org",
+    'style-src': "'self' 'unsafe-inline'", // Allow inline styles
+    'media-src': "'self'"
+  };
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
